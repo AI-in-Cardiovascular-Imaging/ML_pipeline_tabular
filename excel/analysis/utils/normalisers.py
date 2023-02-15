a79d65c9bc14acbdd4967c229c23f77a726859ba
melandur <account@qunit.ch>
2023-02-15 11:12:30 +0100

    wrap ce normalisers

--- excel/analysis/utils/normalisers.py
+++ excel/analysis/utils/normalisers.py
@@ -1,88 +1,60 @@
 import pandas as pd
 from sklearn import preprocessing
 
-from loguru import logger
+
+def data_bubble(func):
+    def wrapper(self, *args):
+        data = args[0]
+        tmp = data[self.target_label]  # keep label col as is
+        x = data.values  # returns a numpy array
+        x_trans = func(self, x)
+        x_trans = pd.DataFrame(x_trans, index=data.index, columns=data.columns)
+        x_trans[self.target_label] = tmp
+        return x_trans
+
+    return wrapper
 
 
 class Normaliser:
     def __init__(self) -> None:
         self.target_label = None
 
+    @data_bubble
     def l1_norm(self, data: pd.DataFrame) -> pd.DataFrame:
         """L1 normalise data"""
-        tmp = data[self.target_label]  # keep label col as is
-        x = data.values  # returns a numpy array
-        x_trans = preprocessing.Normalizer(norm='l1').fit_transform(x.T).T  # l1 normalisation with transposed data
-        data = pd.DataFrame(x_trans, index=data.index, columns=data.columns)
-        data[self.target_label] = tmp
-        return data
+        return preprocessing.Normalizer(norm='l1').fit_transform(data.T).T  # l1 normalisation with transposed data
 
+    @data_bubble
     def l2_norm(self, data: pd.DataFrame) -> pd.DataFrame:
         """L2 normalise data"""
-        tmp = data[self.target_label]  # keep label col as is
-        data = data.drop(self.target_label, axis=1)  # now remove column
-        x = data.values  # returns a numpy array
-        x_trans = preprocessing.Normalizer(norm='l2').fit_transform(x.T).T  # l2 normalisation with transposed data
-        data = pd.DataFrame(x_trans, columns=data.columns)
-        data = data.join(tmp)
-        return data
+        return preprocessing.Normalizer(norm='l2').fit_transform(data.T).T  # l2 normalisation with transposed data
 
+    @data_bubble
     def z_score_norm(self, data: pd.DataFrame) -> pd.DataFrame:
         """Z score data"""
-        tmp = data[self.target_label]  # keep label col as is
-        data = data.drop(self.target_label, axis=1)  # now remove column
-        x = data.values  # returns a numpy array
-        x_trans = preprocessing.StandardScaler().fit_transform(x)
-        data = pd.DataFrame(x_trans, columns=data.columns)
-        data = data.join(tmp)
-        return data
+        return preprocessing.StandardScaler().fit_transform(data)
 
+    @data_bubble
     def min_max_scale(self, data: pd.DataFrame) -> pd.DataFrame:
         """Min max scale data"""
-        tmp = data[self.target_label]  # keep label col as is
-        data = data.drop(self.target_label, axis=1)  # now remove column
-        x = data.values  # returns a numpy array
-        x_trans = preprocessing.MinMaxScaler().fit_transform(x)  # default is 0-1
-        data = pd.DataFrame(x_trans, columns=data.columns)
-        data = data.join(tmp)
-        return data
+        return preprocessing.MinMaxScaler().fit_transform(data)  # default is 0-1
 
+    @data_bubble
     def max_abs_scale(self, data: pd.DataFrame) -> pd.DataFrame:
         """Max abs scale data"""
-        tmp = data[self.target_label]  # keep label col as is
-        data = data.drop(self.target_label, axis=1)  # now remove column
-        x = data.values  # returns a numpy array
-        x_trans = preprocessing.MaxAbsScaler().fit_transform(x)  # default is 0-1
-        data = pd.DataFrame(x_trans, columns=data.columns)
-        data = data.join(tmp)
-        return data
+        return preprocessing.MaxAbsScaler().fit_transform(data)  # default is 0-1
 
+    @data_bubble
     def robust_scale(self, data: pd.DataFrame) -> pd.DataFrame:
         """Robust scale data"""
-        tmp = data[self.target_label]  # keep label col as is
-        data = data.drop(self.target_label, axis=1)  # now remove column
-        x = data.values  # returns a numpy array
-        x_trans = preprocessing.RobustScaler().fit_transform(x)
-        data = pd.DataFrame(x_trans, columns=data.columns)
-        data = data.join(tmp)
-        return data
+        return preprocessing.RobustScaler().fit_transform(data)
 
+    @data_bubble
     def quantile_transform(self, data: pd.DataFrame) -> pd.DataFrame:
         """Quantile transform data"""
-        tmp = data[self.target_label]  # keep label col as is
-        data = data.drop(self.target_label, axis=1)  # now remove column
-        x = data.values  # returns a numpy array
-        x_trans = preprocessing.QuantileTransformer().fit_transform(x)
-        data = pd.DataFrame(x_trans, columns=data.columns)
-        data = data.join(tmp)
-        return data
+        return preprocessing.QuantileTransformer().fit_transform(data)
 
+    @data_bubble
     def power_transform(self, data: pd.DataFrame) -> pd.DataFrame:
         """Power transform data"""
-        tmp = data[self.target_label]  # keep label col as is
-        data = data.drop(self.target_label, axis=1)  # now remove column
-        x = data.values  # returns a numpy array
-        x_trans = preprocessing.PowerTransformer().fit_transform(x)
-        data = pd.DataFrame(x_trans, columns=data.columns)
-        data = data.join(tmp)
-        return data
+        return preprocessing.PowerTransformer().fit_transform(data)
