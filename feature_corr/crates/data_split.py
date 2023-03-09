04d3813dfd1dab3c344cf5d1b10c10674d6404d3
melandur <account@qunit.ch>
2023-03-09 12:21:52 +0100

    refactored data split

--- feature_corr/crates/data_split.py
+++ feature_corr/crates/data_split.py
@@ -7,21 +7,48 @@ class SelectionSplit(DataBorg):
     def __init__(self, config) -> None:
         super().__init__()
         self.config = config
-        self.selection_fracture = self.config.data_split.selection_fracture
+        self.seed = config.meta.seed
+        self.state_name = config.meta.state_name
+        self.learn_task = config.selection.learn_task
+        self.selection_frac = config.selection.selection_frac
+        self.stratify = None
+        self.frame = self.get_store('ephemeral', self.state_name, 'ephemeral')
 
     def __call__(self):
+        self.set_stratification()
+        self.split_data()
 
-        task, stratify = target_statistics(data, self.target_label)
+    def set_stratification(self):
+        """Set stratification"""
+        if self.learn_task == 'binary-classification':
+            target_frame = self.frame[self.config.meta.target_label]
+            self.stratify = target_frame
+        elif self.learn_task == 'multi-classification':
+            raise NotImplementedError('Multi-classification not implemented')
+        elif self.learn_task == 'regression':
+            self.stratify = None
 
-        if 0 < self.explore_frac < 1:
-            explore_data, verification_data = train_test_split(
-                data, stratify=stratify, test_size=1 - self.explore_frac, random_state=self.seed
+    def split_data(self):
+        if 0 < self.set_stratification() < 1:
+            selection_train, verification_train = train_test_split(
+                self.frame,
+                stratify=self.stratify,
+                test_size=1 - self.selection_frac,
+                random_state=self.seed,
             )
-            verification_data_test = None
-        elif self.explore_frac == 0:  # special mode in which entire train data is used for exploration and verification
-            verification_data, verification_data_test = train_test_split(
-                data, stratify=stratify, test_size=0.2, random_state=self.seed
+            self.set_store('data', self.state_name, 'selection_train', selection_train)
+            self.set_store('data', self.state_name, 'verification_train', verification_train)
+            self.set_store('data', self.state_name, 'verification_test', None)
+
+        elif self.selection_frac == 0:  # special mode in which entire train data is used for selection and verification
+            verification_train, verification_test = train_test_split(
+                self.frame,
+                stratify=self.stratify,
+                test_size=0.2,
+                random_state=self.seed,
             )
-            explore_data = verification_data
+            self.set_store('data', self.state_name, 'selection_train', verification_train)
+            self.set_store('data', self.state_name, 'verification_train', verification_train)
+            self.set_store('data', self.state_name, 'verification_test', verification_test)
         else:
-            raise ValueError(f'Value {self.explore_frac} is invalid, must be float in (0, 1)')
+            raise ValueError(f'Value {self.selection_frac} is invalid, must be float in (0, 1)')
--- feature_corr/data_borg/data_borg.py
+++ feature_corr/data_borg/data_borg.py
@@ -58,7 +58,7 @@ class DataBorg:
             return self._ephemeral_frame
         raise ValueError(f'Invalid name -> {data_name}, allowed -> original, ephemeral')
 
-    def set_store(self, data_name: str, state_name: str, step_name: str, frame: pd.DataFrame) -> None:
+    def set_store(self, data_name: str, state_name: str, step_name: str, frame: pd.DataFrame = None) -> None:
         """Sets the store data"""
         if 'frame' in data_name:
             self._frame_store[state_name][step_name] = frame
